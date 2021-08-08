52aa47cce461ea6982dbda2f6b17b35475bba2b0
nhanhuynh2110 <trungnhan21102110@gmail.com>
2021-08-08 09:31:57 +0700

    test action git push deploy azure

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React KEROKU CONNECT GIT  DOCKER1
+          Learn React KEROKU CONNECT GIT Deploy zuure static web
         </a>
       </header>
     </div>
